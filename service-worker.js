f8fca52914267fcb6ab607d6cd58ad7d10a23170
JPDEV <pamplonajp.45@gmail.com>
2025-12-17 16:54:10 +0800

    Updates

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+  return self.clients.claim();
+});
+
+// Handle notification clicks
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
+
+// Keep service worker alive
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
